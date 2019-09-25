ea368c76eb1646a13b0191365a1b9da61f811a91
DeMoore <DeMoore@pods.com>
2019-09-25 00:08:05 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
   onSubmit = () => {
     this.setState({imgUrl: this.state.input})
 
-    fetch('http://localhost:3030/image', {
+    fetch('https://salty-escarpment-80825.herokuapp.com/image', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -94,7 +94,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3030/image', {
+        fetch('https://salty-escarpment-80825.herokuapp.com/image', {
           method: 'put',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3030/signin', {
+        fetch('https://salty-escarpment-80825.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
